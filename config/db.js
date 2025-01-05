9a5a450c2517a08449e83e2ddd00c5ce2935b6bb
Bilal-Sheikh-30 <bilalsheikh3011@gmail.com>
2025-01-05 16:34:06 +0500

    modify db

--- config/db.js
+++ config/db.js
@@ -5,13 +5,6 @@ function connecttoDb() {
         console.log('connected to db');
 
     })
-
-    // mongoose.connect('mongodb+srv://alpha:dbalpha@cluster0.yp7ua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-    //     .then(() => console.log('MongoDB connected'))
-    //     .catch(err => console.error('MongoDB connection error:', err));
-
-
-
 }
 
 module.exports = connecttoDb;
